170741256a9e53548831484be631e9ac207f677f
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2023-01-29 23:55:46 +0900

    server api connect

--- use_scope_button_click.js
+++ use_scope_button_click.js
@@ -1,3 +1,23 @@
+const get_commercial_available_value = async () => {
+    let fetch_return_value = await fetch("http://54.83.101.17:8080/trade/2")
+                    .then((response) => response.json());
+
+    const json_result = await fetch_return_value['result']
+    const commercial_available_value = await json_result['commercial_available'];
+
+    return commercial_available_value;
+}
+
+const get_patent_available_value = async () => {
+    let fetch_return_value = await fetch("http://54.83.101.17:8080/trade/1")
+                    .then((response) => response.json());
+
+    const json_result = await fetch_return_value['result']
+    const patent_available_value = await json_result['patent_available'];
+
+    return patent_available_value;
+}
+
 const detail_view_closed = () => {
     const detail_view_inner = document.getElementById("use_scope_detail_wrapper");
 
@@ -15,7 +35,7 @@ const add_use_scope_detail = () => {
                             상업적 용도
                         </span>
                         <span id="commercial_use_value">
-                            이용가능
+                            
                         </span>
                     </div>
                     <div id="patent">
@@ -23,12 +43,34 @@ const add_use_scope_detail = () => {
                             특허 출원
                         </span>
                         <span id="patent_value">
-                            불가능
+                            
                         </span>
                     </div>
                 </div>
                 </div>
     `;
+
+    const can_commercial_use = get_commercial_available_value();
+    const can_patent_use = get_patent_available_value();
+
+    const commercial_use_value_span = document.getElementById("commercial_use_value");
+    const patent_use_value_span = document.getElementById("patent_value");
+
+    if (can_commercial_use) {
+        commercial_use_value_span.innerText = "이용 가능";
+    } 
+    else {
+        commercial_use_value_span.innerText = "불가능";
+        commercial_use_value_span.style.color = "#FF3120";
+    }
+
+    if (can_patent_use) {
+        patent_use_value_span.innerText = "이용 가능";
+    }
+    else {
+        patent_use_value_span.innerText = "불가능";
+        patent_use_value_span.style.color = "#FF3120";
+    }
 }
 
 const delete_use_scope_detail = () => {
