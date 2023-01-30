e90d90ec9c6d0d90fc5234e1df4558b61e25ec1a
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2023-01-30 16:40:01 +0900

    server connect

--- call_api.js
+++ call_api.js
@@ -1,3 +1,18 @@
+const get_api_address = () => {
+    const url = location.href;
+    
+    const query_string = url.split("?")[1];
+
+    var value_dic = {}
+
+    query_string.split("&").forEach(function(value) {
+        const temp = value.split("=");
+        value_dic[temp[0]] = temp[1];
+    });
+
+    return `http://54.83.101.17:8080/trade/bought_idea/${value_dic["idea_id"]}`
+}
+
 const change_title_text = (title_text) => {
     document.getElementById("title_text").innerText = title_text;
 }
@@ -18,8 +33,52 @@ const change_date_text = (date_text) => {
     document.getElementById("date_text").innerText = `${year}. ${month}. ${day}`;
 }
 
+const change_profile_text = (nickname, idea_cnt) => {
+    document.getElementById("nickname").innerText = nickname;
+    document.getElementById("idea_number").innerText = `판매 아이디어 ${idea_cnt}개`
+}
+
+const change_profile_img = (profile_img) => {
+    document.getElementsByClassName("profile_original_img")[0].src = profile_img;
+}
+
+const change_profile = async (user_id) => {
+    let fetch_return_value = await fetch(`http://54.83.101.17:8080/userinfo/profile/${user_id}`)
+                    .then((response) => response.json());
+
+    const api_result = fetch_return_value['result'][0];
+
+    const nickname = api_result['nickname'];
+    const idea_cnt = api_result['idea_cnt'];
+    const profile_img = api_result['profile'];
+
+    change_profile_text(nickname, idea_cnt);
+    change_profile_img(profile_img);
+}
+
+const change_hashtag = (description) => {
+    const json = JSON.parse(description);
+    const hashtag = json['hashTags'];
+    const categories = document.getElementsByClassName("categories")[0];
+    let add_html = ``;
+
+    hashtag.forEach(function(value, index) {
+        add_html += `
+        <span id="category_${index + 1}" class="category_button">
+            <div id="category_text_${index + 1}" class="category_text_align font_default_set font_weight_regular font_color_black font_size_16">
+                ${value}
+            </div>
+        </span>
+        `
+    });
+
+    console.log(add_html);
+
+    categories.innerHTML = add_html;
+}
+
 const call_api = async () => {
-    let fetch_return_value = await fetch("http://54.83.101.17:8080/trade/1")
+    let fetch_return_value = await fetch(get_api_address())
                     .then((response) => response.json());
 
     const api_result = fetch_return_value['result'];
@@ -28,4 +87,6 @@ const call_api = async () => {
     change_category_text(api_result['category']);
     change_overview_text(api_result['overview']);
     change_date_text(api_result["created"]);
+    change_profile(api_result["user_id"]);
+    change_hashtag(api_result["description"])
 }
